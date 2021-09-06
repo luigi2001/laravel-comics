b58c9e8eea283f4d705cdcb3cc7295b1a9baf2d8
luigi2001 <luigitanga@virgilio.it>
2021-09-06 18:24:35 +0200

    add header and footer

---
+++ resources/views/parti/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="logo">
+        <img src="{{ asset('img/dc-logo.png') }}" alt=" dc logo">
+    </div>
+    <div class="nav-bar">
+        <ul>
+            <li><a href="">characters</a></li>
+            <li><a href="">comics</a></li>
+            <li><a href="">movies</a></li>
+            <li><a href="">tv</a></li>
+            <li><a href="">games</a></li>
+            <li><a href="">videos</a></li>
+        </ul>
+    </div>
+</header>
