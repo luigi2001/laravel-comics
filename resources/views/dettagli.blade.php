1f8893f221671c025f3dedd85559ba4e4d2b070d
luigi2001 <luigitanga@virgilio.it>
2021-09-07 18:13:26 +0200

    add dettaglio card

--- resources/views/dettagli.blade.php
+++ resources/views/dettagli.blade.php
@@ -5,9 +5,12 @@
 
 @section('content')
 <div class="dettaglio">
-    <img src="{{ $comic('thumb') }}" alt="">
-    <h1></h1>
-    <p></p>
-    <h3></h3>
+    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+    <h1>{{ $comic['title'] }}</h1>
+    <p>{{ $comic['description'] }}</p>
+    <h3>{{ $comic['price'] }}</h3>
+    <h3>{{ $comic['series'] }}</h3>
+    <h3>{{ $comic['sale_date'] }}</h3>
+    <h3>{{ $comic['type'] }}</h3>
 </div>
 @endsection
